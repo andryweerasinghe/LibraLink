6db89c6b8378475e3683ed48fc5fb11185f7c866
andryweerasinghe <andryweerasinghe@gmail.com>
2024-02-29 22:15:12 +0530

    FactoryConfiguration and entities

---
+++ src/main/java/lk/ijse/libraLink/config/FactoryConfiguration.java
@@ -0,0 +1,33 @@
+/*
+ * Author  : Mr.electrix
+ * Project : hibernateMapping
+ * Date    : 2/2/24
+
+ */
+
+package lk.ijse.libraLink.config;
+
+import lk.ijse.libraLink.entity.Book;
+import lk.ijse.libraLink.entity.Branch;
+import lk.ijse.libraLink.entity.Transaction;
+import lk.ijse.libraLink.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(User.class)
+                .addAnnotatedClass(Book.class).addAnnotatedClass(Branch.class).addAnnotatedClass(Transaction.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null)?factoryConfiguration=new FactoryConfiguration():factoryConfiguration;
+    }
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
